2a35548dc8b787bf9979e15d3be77584d09022b0
TatianaKucheba <tanya.kucheba@gmail.com>
2023-05-02 04:04:40 +0600

    feat: add functional for keyboard

--- index.js
+++ index.js
@@ -1,11 +1,84 @@
 import KEYS from "./keys.js";
 
+class KeyboardKey {
+  constructor(key, keyboard) {
+    this.isServiceKey = key.group === "service";
+
+    this.keyboard = keyboard;
+    this.key = key;
+    this.el = document.createElement("div");
+    this.el.classList.add("key");
+    this.el.textContent = this.keyText;
+    this.el.dataset.code = key.code;
+    this.shift = false;
+    if (key.group === "service") {
+      this.el.classList.add("service");
+    }
+    this.el.addEventListener("click", (e) => {
+      keyboard.emit("keydown", {
+        ...e,
+        code: this.key.code,
+        key: this.keyText,
+      });
+      keyboard.emit("keyup", {
+        ...e,
+        code: this.key.code,
+        key: this.keyText,
+      });
+
+      this.el.classList.add("active");
+      setTimeout(() => {
+        this.el.classList.remove("active");
+      }, 200);
+    });
+
+    keyboard.container.appendChild(this.el);
+    keyboard.on("layout", () => {
+      this.renderText();
+    });
+    keyboard.on("shift", (event) => {
+      this.shift = event.shiftKey;
+      this.renderText();
+    });
+  }
+  get isUppercase() {
+    return this.shift || this.keyboard.caps;
+  }
+  get layout() {
+    return this.keyboard.layout;
+  }
+  get keyText() {
+    if (this.isUppercase && this.layout === "En") {
+      return this.isServiceKey ? this.key.key : this.key.shiftKeyEN;
+    }
+    if (this.isUppercase && this.layout === "Ru") {
+      return this.isServiceKey ? this.key.key : this.key.shiftKeyRu;
+    }
+    if (this.layout === "En") {
+      return this.isServiceKey ? this.key.key : this.key.key;
+    }
+    if (this.layout === "Ru") {
+      return this.isServiceKey ? this.key.key : this.key.keyRu;
+    }
+  }
+  renderText() {
+    this.el.innerText = this.keyText;
+  }
+}
+
 class VirtualKeyboard {
   constructor() {
     // создаем контейнер для клавиатуры и текстового поля
+    this.layouts = ["Ru", "En"];
     this.wrapper = document.createElement("div");
     this.wrapper.classList.add("keyboard-wrapper");
 
+    this.description = document.createElement("div");
+    this.description.classList.add("keyboard-description");
+    this.description.innerText =
+      "Клавиатура была создана в Windows10 \n Для смены раскладки клавиатуры нажми ALT+SHIFT для Windows, OPTION+SHIFT для MacOs ";
+
+    this.wrapper.append(this.description);
     // создаем текстовое поле
     this.input = document.createElement("div");
     this.input.type = "text";
@@ -17,21 +90,9 @@ class VirtualKeyboard {
     // создаем контейнер для клавиатуры
     this.container = document.createElement("div");
     this.container.classList.add("keyboard-container");
-
     // создаем элементы клавиатуры
     KEYS.forEach((key) => {
-      const keyEl = document.createElement("div");
-      keyEl.classList.add("key");
-      keyEl.textContent = key.key;
-      keyEl.dataset.code = key.code;
-
-      keyEl.addEventListener("click", () => {
-        this.input.innerText += key.key;
-        this.emit("keydown", key.code);
-        this.emit("keyup", key.code);
-      });
-
-      this.container.appendChild(keyEl);
+      new KeyboardKey(key, this);
     });
 
     // добавляем контейнер клавиатуры на страницу
@@ -42,41 +103,95 @@ class VirtualKeyboard {
 
     // добавляем обработчик события keydown на документе
     document.addEventListener("keydown", (event) => {
-      switch (event.key.code) {
-        case "Tab":
-          this.insertText("\t");
-          break;
-        case "Enter":
-          this.insertText("\n");
-          break;
-        case "ArrowUp":
-          this.moveCursor("up");
-          break;
-        case "ArrowDown":
-          this.moveCursor("down");
-          break;
-        case "ArrowLeft":
-          this.moveCursor("left");
-          break;
-        case "ArrowRight":
-          this.moveCursor("right");
-          break;
-        case "Backspace":
-          this.deleteChar("back");
-      }
-      const keyEl = this.container.querySelector(`[data-code="${event.code}"]`);
-      if (keyEl) {
-        keyEl.classList.add("active");
-      }
+      this.emit("keydown", event);
     });
-
+    this.on("keydown", (event) => this.keydown(event));
     // добавляем обработчик события keyup на документе
     document.addEventListener("keyup", (event) => {
       const keyEl = this.container.querySelector(`[data-code="${event.code}"]`);
       if (keyEl) {
         keyEl.classList.remove("active");
       }
+      if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
+        this.emit("shift", event);
+      }
     });
+  }
+
+  keydown(event) {
+    if (event.altKey && event.shiftKey) {
+      event?.preventDefault();
+      this.layout === this.layouts[0]
+        ? (this.layout = this.layouts[1])
+        : (this.layout = this.layouts[0]);
+      this.emit("layout");
+      return;
+    }
+    switch (event.code) {
+      case "ShiftLeft":
+      case "ShiftRight":
+        this.emit("shift", event);
+      case "ControlLeft":
+      case "ControlRight":
+      case "Alt":
+      case "AltLeft":
+      case "AltRight":
+      case "Delete":
+        break;
+      case "Tab":
+        this.insertText(`    `, event);
+        break;
+      case "Space":
+        this.insertText(` `, event);
+        break;
+      case "Enter":
+        this.insertText("\n", event);
+        break;
+      case "ArrowUp":
+        this.insertText("↑", event);
+        break;
+      case "ArrowDown":
+        this.insertText("↓", event);
+        break;
+      case "ArrowLeft":
+        this.insertText("←", event);
+        break;
+      case "ArrowRight":
+        this.insertText("→", event);
+        break;
+      case "Backspace":
+        this.deleteChar(event);
+        break;
+      case "CapsLock":
+        this.caps = !this.caps;
+        console.log(this.caps);
+        this.emit("shift", event);
+        break;
+      default:
+        event && event.preventDefault && event.preventDefault();
+        this.insertText(event.key);
+    }
+    const keyEl = this.container.querySelector(`[data-code="${event.code}"]`);
+    if (keyEl) {
+      keyEl.classList.add("active");
+    }
+  }
+  get layout() {
+    const layout = window.localStorage.getItem("layout");
+    return layout ? layout : "Ru";
+  }
+
+  set layout(layout) {
+    window.localStorage.setItem("layout", layout);
+  }
+
+  get caps() {
+    const caps = window.localStorage.getItem("caps");
+    return !!JSON.parse(caps);
+  }
+
+  set caps(caps) {
+    window.localStorage.setItem("caps", caps);
   }
 
   on(eventName, callback) {
@@ -84,8 +199,14 @@ class VirtualKeyboard {
     this.events[eventName] = this.events[eventName] || [];
     this.events[eventName].push(callback);
   }
-  insertText(text) {
-    /*this.input.insertAdjacentText('beforeend', text);*/
+
+  deleteChar(event) {
+    event && event.preventDefault && event.preventDefault();
+    this.input.innerText = this.input.innerText.slice(0, -1);
+  }
+
+  insertText(text, event) {
+    event && event.preventDefault && event.preventDefault();
     this.input.innerText += text;
   }
 
